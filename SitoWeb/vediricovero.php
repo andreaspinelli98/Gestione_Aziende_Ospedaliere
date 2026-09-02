f090a50c6c7503cf33def6731c357ded16835f51
Andrea Spinelli <andreaspin4@gmail.com>
2026-09-02 11:34:08 +0200

    Refactor comments in vediricovero.php

--- SitoWeb/vediricovero.php
+++ SitoWeb/vediricovero.php
@@ -8,8 +8,8 @@
         table, th, td {            
             border: 1px solid black; 
             border-collapse: collapse;
-            text-align: left; /*mette "nome" di fianco alla riga*/
-            padding: 4px; /*aumenta dimensione testo*/
+            text-align: left; 
+            padding: 4px; 
         }
     </style>
 </head>
@@ -20,7 +20,7 @@
         if (isset($_POST['tessanit'])) {
             $ts = $_POST['tessanit'];
 
-            $conn = pg_connect("host=localhost port=5432 dbname=Progetto user=pgadmin password=unimi");
+            $conn = pg_connect("host=localhost port=5432 dbname=Progetto user=f password=f"); //cambiati user e password per sicurezza
             if (!$conn) {
                 echo "Errore di connessione al database.";
                 echo "<p> Clicca <a href='ricovero.php'>qui</a> per tornare indietro</p>";
@@ -28,16 +28,16 @@
                 exit;
             } else {
                 echo "Connessione riuscita.<br/>";
-                $query = "SELECT * FROM ricovero WHERE tessanit='$ts'";//fare attenzione che deve avere apici
+                $query = "SELECT * FROM ricovero WHERE tessanit='$ts'";
                 $result = pg_query($conn, $query);
                 
-                if (!$result) { // La query ha generato errori
+                if (!$result) { 
                     echo "Si è verificato un errore.<br/>";
                     echo pg_last_error($conn);
                     echo "<p> Clicca <a href='ricovero.php'>qui</a> per tornare indietro</p>";
                     echo "<p> Clicca <a href='home.html'>qui</a> per tornare alla Home</p>";
                     exit();
-                } else { // La query non ha generato errori
+                } else {
                     echo '<br><table>
                         <tr>
                             <th>Codice ospedale</th>
@@ -67,8 +67,8 @@
                 };
             };
             exit();
-            pg_close($conn); // Chiudi la connessione al database
-        } else { // Non sono stati passati correttamente i dati
+            pg_close($conn); 
+        } else { 
             echo "ccc". $ts;
             echo "Non risultano dati passati<br>";
             echo "<p> Clicca <a href='ricovero.php'>qui</a> per tornare indietro</p>";
